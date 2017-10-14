69bd20fe5f99c8715d0eaa091821a0a0548f6b96
danielmoralesp <daniel_moralesp@hotmail.com>
2017-10-14 13:07:53 -0500

    Devise

--- app/controllers/request_example_controller.rb
+++ app/controllers/request_example_controller.rb
@@ -1,9 +1,20 @@
 class RequestExampleController < ApplicationController
-  def curl_get_example
-    render plain: 'Thanks for sending a GET request with cURL!'
+
+  before_filter :authenticate_user!, except: [:index, :show]
+
+  
+  #def curl_get_example
+  #  render plain: 'Thanks for sending a GET request with cURL!'
+  #end
+
+  #def curl_post_example
+  #  render text: "Thanks for sending a POST request with cURL! Payload: #{request.body.read}"
+  #end
+
+  def index
   end
 
-  def curl_post_example
-    render plain: "Thanks for sending a POST request with cURL! Payload: #{request.body.read}"
+  def create
+    render json: params
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  match '/curl_example' => 'request_example#curl_get_example', via: :get
-  match '/curl_example' => 'request_example#curl_post_example', via: :post
+  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
+  root to: 'request_example#index'
+  match '/request' => 'request_example#create', via: :post
+  match '/request' => 'request_example#create', via: :get
+
+  #match '/curl_example' => 'request_example#curl_get_example', via: :get
+  #match '/curl_example' => 'request_example#curl_post_example', via: :post
 end
